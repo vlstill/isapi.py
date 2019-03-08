fdba65b36c78ee328ca6e89f45e1d3154ec18bd6
Vladimir Still <xstill@fi.muni.cz>
2019-03-08 15:41:50 +0100

    Documentation, refactoring

--- isapi.py
+++ isapi.py
@@ -38,6 +38,9 @@ class Notebook:
 
 
 def getkey(path) -> Optional[str]:
+    """
+    Try to parse api key from given directory from file name "isapikey".
+    """
     try:
         with open(os.path.join(path, "isapikey"), "r") as f:
             return f.read().strip()
@@ -64,125 +67,6 @@ def __extract(node : ET.Element, *args) -> str:
     return t
 
 
-class IS:
-    STARNUM = re.compile(r"\*[0-9]*\.?[0-9]*")
-
-    def __init__(self, course : str = None, faculty = None, api_key : Optional[str] = None) -> None:
-        if api_key is None:
-            api_key = getkey(".")
-        self.__DEFARGS = {"klic": api_key,
-                          "fakulta": "1433",
-                         }
-        if course is not None:
-            self.__DEFARGS['kod'] = course
-        if faculty is not None:
-            self.__DEFARGS['fakulta'] = faculty
-
-    def __raw_req(self, args : dict) -> ET.Element:
-        for k, v in self.__DEFARGS.items():
-            if k not in args:
-                args[k] = v
-        base_url = "https://is.muni.cz/export/pb_blok_api"
-        req = requests.post(base_url, args)
-        if req.status_code != 200:
-            raise ISAPIException("Error {} {}".format(req.status_code, req.reason))
-        x = ET.fromstring(req.text)
-        if x.tag == "CHYBA":
-            raise ISAPIException(x.text)
-        return x
-
-    def get_notebooks(self, course : str) -> List[Notebook]:
-        data = self.__raw_req({"kod": course, "operace": "bloky-seznam"})
-        out = []
-        for child in data:
-            out.append(Notebook(__extract(child, "JMENO"),
-                                int(__extract(child, "TYP_ID")),
-                                __extract(child, "ZKRATKA")))
-        return out
-
-    def course_info(self, course : str) -> Course:
-        data = self.__raw_req({"kod": course, "operace": "predmet-info"})
-        teachers = []
-        for tutor in __get_node(data, "VYUCUJICI_SEZNAM"):
-            teachers.append(Person(__extract(tutor, "JMENO"),
-                                   __extract(tutor, "PRIJMENI"),
-                                   int(__extract(tutor, "UCO"))))
-        return Course(__extract(data, "FAKULTA_ZKRATKA_DOM"),
-                      __extract(data, "NAZEV_PREDMETU"),
-                      teachers)
-
-    def get_attendance_notebooks(self, course : str) -> List[Notebook]:
-        return [x for x in self.get_notebooks(course) if x.type == 5]
-
-    def load_notebook(self, course : str, shortcut : str)\
-                      -> Dict[str, Tuple[str, str]]:
-        """
-        returns a dict of mappings UCO -> (contents, last_change)
-        """
-        data = self.__raw_req({"kod": course, "operace": "blok-dej-obsah",
-                                    "zkratka": shortcut})
-        out : Dict[str, Tuple[str, str]] = {}
-        for child in data:
-            assert child.tag == "STUDENT"
-            skip = False
-            for c in child:
-                if c.tag == "NEMA_POZN_BLOK":
-                    skip = True
-            if skip:
-                continue
-
-            uco = __extract(child, "UCO")
-            contents = __extract(child, "OBSAH")
-            change = __extract(child, "ZMENENO")
-            assert uco not in out.keys()
-
-            out[uco] = (contents, change)
-        return out
-
-    def notebook_to_points(self, contents):
-        def ft(x):
-            if x == "":
-                return 0
-            return float(x)
-        return sum([ft(x.group()[1:]) for x in STARNUM.finditer(contents)])
-
-    def load_points(self, course, shortcut):
-        """
-        returns a dict of mappings UCO -> (pointrs, last_change)
-        """
-        return dict([(k, (notebook_to_points(v[0]), v[1]))
-                     for k, v in load_notebook(course, shortcut).items()])
-
-    def students_list(self, course : str) -> List[Person]:
-        data = self.__raw_req({"kod": course, "operace": "predmet-seznam"})
-        students : List[Person] = []
-        for st in data:
-            students.append(Person(__extract(st, "JMENO"),
-                                   __extract(st, "PRIJMENI"),
-                                   int(__extract(st, "UCO"))))
-        return students
-
-    def create_notebook(self, course : str, name : str, short : str) -> bool:
-        try:
-            self.__raw_req({"kod": course, "operace": "blok-novy",
-                                 "jmeno": name, "zkratka": short,
-                                 "nahlizi": "n", "nedoplnovat": "n", "statistika": "n"})
-            return True
-        except ISAPIException:
-            return False
-
-    def store(self, course : str, short : str, uco : str, contents : str,
-              timestamp : Optional[datetime.datetime] = None, overwrite = False)\
-              -> None:
-        args = {"kod": course, "operace": "blok-pis-student-obsah",
-                "zkratka": short, "uco": uco, "obsah": contents}
-        if overwrite:
-            args['prepis'] = 'a'
-        if timestamp is not None:
-            args['poslzmeneno'] = serialize_date(timestamp)
-        self.__raw_req(args)
-
-
 def parse_date(date : str) -> datetime.datetime:
     raw = datetime.datetime(year = int(date[:4]),
                             month = int(date[4:6]),
@@ -197,3 +81,150 @@ def parse_date(date : str) -> datetime.datetime:
 def serialize_date(date : datetime.datetime) -> str:
     return "%04d%02d%02d%02d%02d%02d" % (date.year, date.month, date.day,
                                          date.hour, date.minute, date.second)
+
+
+class Entry:
+    STARNUM = re.compile(r"\*[0-9]*\.?[0-9]*")
+
+    def __init__(self, text : str, timestamp : Optional[datetime.datetime] = None) -> None:
+        self.text = text
+        self.timestamp = timestamp
+
+    def points(self) -> float:
+        def ft(x):
+            if x == "":
+                return 0
+            return float(x)
+        return sum([ft(x.group()[1:]) for x in Entry.STARNUM.finditer(self.text)])
+
+
+class Connection:
+
+    def __init__(self, course : str = None, faculty = None, api_key : Optional[str] = None) -> None:
+        """
+        Initialize a new instance.
+        Course has to be set.
+        The api_key has to either be in isapikey file in the working directory,
+        or provided.
+        The default faculty is FI.
+        """
+        if api_key is None:
+            api_key = getkey(".")
+        self.__DEFARGS = {"klic": api_key,
+                          "fakulta": "1433",
+                         }
+        if course is not None:
+            self.__DEFARGS['kod'] = course
+        if faculty is not None:
+            self.__DEFARGS['fakulta'] = faculty
+
+    def __raw_req(self, args : dict) -> ET.Element:
+        for k, v in self.__DEFARGS.items():
+            if k not in args or args[k] is None:
+                args[k] = v
+        base_url = "https://is.muni.cz/export/pb_blok_api"
+        req = requests.post(base_url, args)
+        if req.status_code != 200:
+            raise ISAPIException("Error {} {}".format(req.status_code, req.reason))
+        x = ET.fromstring(req.text)
+        if x.tag == "CHYBA":
+            raise ISAPIException(x.text)
+        return x
+
+    def notebooks(self, course : Optional[str] = None) -> List[Notebook]:
+        """
+        Get a list of notebooks for a given course.
+        If the course was set in constructor, it does not need to be set here.
+        """
+        data = self.__raw_req({"kod": course, "operace": "bloky-seznam"})
+        out = []
+        for child in data:
+            out.append(Notebook(__extract(child, "JMENO"),
+                                int(__extract(child, "TYP_ID")),
+                                __extract(child, "ZKRATKA")))
+        return out
+
+    def course_info(self, course : Optional[str] = None) -> Course:
+        """
+        Get information about a course, this includes lists of teachers.
+        """
+        data = self.__raw_req({"kod": course, "operace": "predmet-info"})
+        teachers = []
+        for tutor in __get_node(data, "VYUCUJICI_SEZNAM"):
+            teachers.append(Person(__extract(tutor, "JMENO"),
+                                   __extract(tutor, "PRIJMENI"),
+                                   int(__extract(tutor, "UCO"))))
+        return Course(__extract(data, "FAKULTA_ZKRATKA_DOM"),
+                      __extract(data, "NAZEV_PREDMETU"),
+                      teachers)
+
+    def attendance_notebooks(self, course : Optional[str] = None) -> List[Notebook]:
+        """
+        Get list of notebooks used for attendance tracking.
+        """
+        return [x for x in self.notebooks(course) if x.type == 5]
+
+    def notebook(self, shortcut : str, course : Optional[str] = None)\
+                      -> Dict[int, Entry]:
+        """
+        Returns a mappings UCO -> Entry
+        """
+        data = self.__raw_req({"kod": course, "operace": "blok-dej-obsah",
+                                    "zkratka": shortcut})
+        out : Dict[int, Entry] = {}
+        for child in data:
+            assert child.tag == "STUDENT"
+            skip = False
+            for c in child:
+                if c.tag == "NEMA_POZN_BLOK":
+                    skip = True
+            if skip:
+                continue
+
+            uco = int(__extract(child, "UCO"))
+            contents = __extract(child, "OBSAH")
+            change = __extract(child, "ZMENENO")
+            assert uco not in out.keys()
+
+            out[uco] = Entry(contents, parse_date(change))
+        return out
+
+    def students_list(self, course : Optional[str] = None) -> List[Person]:
+        """
+        Get a list of students.
+        """
+        data = self.__raw_req({"kod": course, "operace": "predmet-seznam"})
+        students : List[Person] = []
+        for st in data:
+            students.append(Person(__extract(st, "JMENO"),
+                                   __extract(st, "PRIJMENI"),
+                                   int(__extract(st, "UCO"))))
+        return students
+
+    def create_notebook(self, name : str, short : str, course : Optional[str] = None) -> bool:
+        """
+        Creates a new notebook given its name and shortcut.
+        """
+        try:
+            self.__raw_req({"kod": course, "operace": "blok-novy",
+                            "jmeno": name, "zkratka": short,
+                            "nahlizi": "n", "nedoplnovat": "n", "statistika": "n"})
+            return True
+        except ISAPIException:
+            return False
+
+    def store(self, short : str, uco : int, entry : Entry, course : Optional[str],
+              overwrite = False)\
+              -> None:
+        """
+        Writes given (modified) entry to IS, the update is by default works
+        only if timestamp in the entry matches timestamp in IS or if there is
+        no entry in IS. Optionally overwritting existing data unconditionally.
+        """
+        args = {"kod": course, "operace": "blok-pis-student-obsah",
+                "zkratka": short, "uco": str(uco), "obsah": entry.text}
+        if overwrite:
+            args['prepis'] = 'a'
+        if entry.timestamp is not None:
+            args['poslzmeneno'] = serialize_date(entry.timestamp)
+        self.__raw_req(args)
